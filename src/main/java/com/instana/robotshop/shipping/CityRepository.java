133c0ac3769e3ba220ae3d686d4e723857484f5a
Raghu K <raghuk.vit@gmail.com>
2025-07-04 06:27:38 +0530

    Revert "RoboShop Project"
    This reverts commit 0eb7ba2868c07bf84baf06ddd7bb4f9c55a3a98b.

--- src/main/java/com/instana/robotshop/shipping/CityRepository.java
+++ src/main/java/com/instana/robotshop/shipping/CityRepository.java
@@ -2,20 +2,16 @@ package com.instana.robotshop.shipping;
 
 import java.util.List;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CityRepository extends CrudRepository<City, Long> {
-    @Cacheable("cities")
     List<City> findByCode(String code);
 
-    @Cacheable("matches")
     @Query(
         value = "select c from City c where c.code = ?1 and c.city like ?2%"
     )
     List<City> match(String code, String text);
 
-    @Cacheable("city")
     City findById(long id);
 }
--- src/main/java/com/instana/robotshop/shipping/CodeRepository.java
+++ src/main/java/com/instana/robotshop/shipping/CodeRepository.java
@@ -2,12 +2,10 @@ package com.instana.robotshop.shipping;
 
 import java.util.List;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface CodeRepository extends PagingAndSortingRepository<Code, Long> {
 
-    @Cacheable("codes")
     Iterable<Code> findAll();
 
     Code findById(long id);
