700157f874aa8feb828fde0abda904692392373e
Raghu K <raghuk.vit@gmail.com>
2025-08-14 07:25:55 +0530

    RoboShop Project

--- db/run.sh
+++ db/run.sh
@@ -1,4 +1,4 @@
-mysql $DB_HOST -u${DB_USER} -p${DB_PASS} <schema.sql
-mysql $DB_HOST -u${DB_USER} -p${DB_PASS} <app-user.sql
-mysql $DB_HOST -u${DB_USER} -p${DB_PASS} <master-data.sql
+mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} <schema.sql
+mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} <app-user.sql
+mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} <master-data.sql
 
